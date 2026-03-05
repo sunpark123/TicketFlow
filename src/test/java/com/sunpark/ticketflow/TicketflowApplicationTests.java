9fd60da625af49f9272e735a3d26f730fc5ab9b3
석현07 <gokpoo31@gmail.com>
2026-03-06 00:37:02 +0900

    spring boot setting

---
+++ src/test/java/com/sunpark/ticketflow/TicketflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunpark.ticketflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
